f48f4257fc59190a59be5e5f314e57289ac6c599
Shreyas Jadhav <126581827+Shreejgithub@users.noreply.github.com>
2024-02-10 20:02:08 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
     const parent = React.createElement("div", { id: "parent"},
     [React.createElement("div", { id: "child"},[
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag")
+        React.createElement("h1", {}, "This is namaste react"), 
+        React.createElement("h2", {}, "by Shreyas Jadhav")
     ]),
     React.createElement("div", { id: "child2"},[
         React.createElement("h1", {}, "I am an h1 tag"), 
--- REDME.md
+++ REDME.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+
+# parcel
+-Dev Build
+-Local server
+-HMR = Hot Module Replacement
+-File Watching Algorithm = written in C++
+-Catching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code splitting
+-Differential bundling
+-Diagnostic
+-Error Handling
+-HTTPs
+Trees Shaking - remove unsend code
+-Different dev and
